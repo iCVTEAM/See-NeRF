04c94ca3b6ffaa7540a7d661e003b8aa0424800c
Yunshan Qi <46859922+171164558@users.noreply.github.com>
2026-07-14 22:36:09 +0800

    Add files via upload

--- data/synthetic/1-raw2processed.py
+++ data/synthetic/1-raw2processed.py
@@ -14,6 +14,7 @@ def tonemap_exp(img, exp_time):
 scenes = ["BathRoom", "CatRoom", "DinningRoom", "DogRoom", "Sofa", "Sponza", "ToyRoom", "WarmRoom"]
 
 
+
 # Generate the images for training, testing, and event simulation
 print("------Stage 1: Training, Testing, and Event Simulation Image Folders Generation------")
 
@@ -41,11 +42,8 @@ for scene in scenes:
             # rgba to rgb, 22 is brightness factor of white bg
             img_hdr = img_hdr[..., :3] * img_hdr[..., -1:] + (1. - img_hdr[..., -1:]) * 22
             img_hdr = cv2.resize(img_hdr, (400, 400))
-            if j == 0 or j == 16:
-                img_hdr_blur = img_hdr_blur + img_hdr / 2
-            else:
-                img_hdr_blur = img_hdr_blur + img_hdr
-        img_hdr_blur = img_hdr_blur / 16
+            img_hdr_blur = img_hdr_blur + img_hdr
+        img_hdr_blur = img_hdr_blur / 17
         # generate LDR blurry training images with exposure time t_0 ~ t_4
         for t in range(5):
             img_ldr_blur = tonemap_exp(img_hdr_blur, 2 ** (2 * (t - 2)))
